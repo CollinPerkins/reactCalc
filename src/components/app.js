e638171a54bac37053c82f39b9b7e568bcf3576b
Collin Perkins <collinperkinsweb@gmail.com>
2017-01-25 09:49:40 -0600

    first commit

---
+++ src/components/app.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+    };
+  }
+
+  componentDidMount() {
+
+    
+
+  }
+
+  render() {
+    return (
+      <div>
+        <button className="btn">1</button>
+        <button className="btn">1</button>
+        <button className="btn">1</button>
+        <button className="btn">1</button>
+        <button className="btn">1</button>
+        <button className="btn">1</button>
+        <button className="btn">1</button>
+      </div>
+    );
+  }
+}

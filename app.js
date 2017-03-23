097cc8babd3ce916465cbf3d6778fb3c4bcbe6c5
Collin Perkins <collinperkinsweb@gmail.com>
2017-03-23 11:09:04 -0500

    Enable wwwhisper authorization

--- app.js
+++ app.js
@@ -4,11 +4,13 @@ const port = process.env.PORT || 3030;
 const app = express();
 const cors = require('cors');
 const axios = require('axios');
+var wwwhisper = require('connect-wwwhisper');
 
+app.use(wwwhisper());
+app.use(wwwhisper(false));
 app.use(express.static(__dirname + '/'));
 
 app.use(cors());
-
 app.get('/getQuote', (req, res) => {
   axios.get("http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json").then(data => {
     res.json(data);
